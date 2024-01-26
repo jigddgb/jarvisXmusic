7534b470c5a8b87d8be0f8de73ecbfd1c4237d61
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-01-26 23:37:14 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -31,10 +31,10 @@ async def make_carbon(code):
     output_image.name = "carbon.png"
     return output_image
 
-@app.on_message(filters.command("ping", prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & ~BANNED_USERS)
+@app.on_message(filters.command("pong", prefixes=["/", "!", "%", ",", ".", "@", "#"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    PING_IMG_URL = "https://telegra.ph/file/e7764382d4a2b06c1705c.jpg"
+    PING_IMG_URL = "https://telegra.ph/file/341ff896a29ab9c337c8d.mp4"
     captionss = "**🥀ᴘɪɴɢɪɴɢ ᴏᴜʀ sᴇʀᴠᴇʀ ᴡᴀɪᴛ.**"
     response = await message.reply_photo(PING_IMG_URL, caption=(captionss))
     await asyncio.sleep(1)
