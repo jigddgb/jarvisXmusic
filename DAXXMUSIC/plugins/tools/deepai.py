2e749183ab45a9eebc177a24309e21d559fcb054
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-01-29 04:01:19 +0530

    Update deepai.py

--- DAXXMUSIC/plugins/tools/deepai.py
+++ DAXXMUSIC/plugins/tools/deepai.py
@@ -7,7 +7,7 @@ api_key ="995e3267-53f1-496e-82e3-39754eab99dc"
 
 
 
-@app.on_message(filters.command(["deep" , ],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+@app.on_message(filters.command(["deep"],  prefixes=["+", ".", "/"]))
 async def deepchat(app: app, message):
     name = message.from_user.first_name
     try:
